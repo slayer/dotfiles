e0091ff23bcac3c4e3d6a5146fd96b699dfd18ef
Vladislav Moskovets <github@vlad.org.ua>
2009-06-03 00:05:18 +0300

    added bash completion for rake

--- bashrc.d/rails.sh
+++ bashrc.d/rails.sh
@@ -4,3 +4,13 @@
 alias sc='script/console'
 alias ss='script/server' # start up the beast; use "ss -d" to detach
 alias sg='script/generate'
+
+
+
+export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
+
+_rakecomplete() {
+	COMPREPLY=($(compgen -W "`rake -s -T | awk '{{print $2}}'`" -- ${COMP_WORDS[COMP_CWORD]}))
+	return 0
+}
+complete -o default -o nospace -F _rakecomplete rake
