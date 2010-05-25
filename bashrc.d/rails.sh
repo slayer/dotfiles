84754fc1b400e678b37c126e7af069b7a373bd49
Vladislav Moskovets <github@vlad.org.ua>
2010-05-25 11:43:00 +0300

    COMP_WORDBREAKS fix

--- bashrc.d/rails.sh
+++ bashrc.d/rails.sh
@@ -7,10 +7,6 @@ alias sg='script/generate'
 
 
 
-export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
+#export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
+#export COMP_WORDBREAKS='"\'><=;|&('
 
-_rakecomplete() {
-	COMPREPLY=($(compgen -W "`rake -s -T | awk '{{print $2}}'`" -- ${COMP_WORDS[COMP_CWORD]}))
-	return 0
-}
-complete -o default -o nospace -F _rakecomplete rake
