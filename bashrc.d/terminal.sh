438972e9b7bd627072762a29347556bd907556d4
Vladislav Moskovets <github@vlad.org.ua>
2011-07-12 11:21:16 +0300

    terminal.sh fixes

--- bashrc.d/terminal.sh
+++ bashrc.d/terminal.sh
@@ -18,13 +18,13 @@ shopt -s checkwinsize
 shopt -s histappend
 
 # Disable Ctrl-S
-stty stop '' 2>/dev/null
+stty stop '' 2>/dev/null || true
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(lesspipe)"
 
 # make history more friendly
-bind '"\e[A":history-search-backward'
-bind '"\e[B":history-search-forward'
+bind '"\e[A":history-search-backward' 2>/dev/null  || true
+bind '"\e[B":history-search-forward' 2>/dev/null   || true
 
 
