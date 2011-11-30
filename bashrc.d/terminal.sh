4bc07ead8979b00c98a020d2085d33c0e672990e
Vladislav Moskovets <github@vlad.org.ua>
2011-11-30 03:59:58 +0200

    revert terminal.sh

--- bashrc.d/terminal.sh
+++ bashrc.d/terminal.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # Calculate a short checksum of the real hostname to determine a unique color
-if [[ $TERM =~ "256color" ]]; then
-    host_color="38;5;$((16 + $(hostname | cksum | cut -c1-3) % 256))";
-else
-    host_color="1;$((31 + $(hostname | cksum | cut -c1-3) % 6))";
-fi
+#if [[ $TERM =~ "256color" ]]; then
+    #host_color="38;5;$((16 + $(hostname | cksum | cut -c1-3) % 256))";
+#else
+    #host_color="1;$((31 + $(hostname | cksum | cut -c1-3) % 6))";
+#fi
 
 case "$TERM" in
 	xterm*|rxvt*|screen*)
     if [ `id -u` = 0 ]; then
         PS1='${debian_chroot:+($debian_chroot)}\[\033[0;31m\]\u\[\033[00m\]@\[\033[0;32m\]\h\[\033[00m\] \[\033[00m\]\w\[\033[00m\] \$ '
     else
-        PS1='${debian_chroot:+($debian_chroot)}\[\033[0;32m\]\u@\[\033[0;'$host_color'm\]\h\[\033[00m\] \[\033[00m\]\w\[\033[00m\] \$ '
+        PS1='${debian_chroot:+($debian_chroot)}\[\033[0;32m\]\u@\h\[\033[00m\] \[\033[00m\]\w\[\033[00m\] \$ '
     fi
 	#PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007"'
     TITLEBAR='\[\033]0;\u@\h:\w\007\]'
