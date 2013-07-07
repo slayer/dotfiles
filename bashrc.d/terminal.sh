2e7d56da2e88477dda8d89dd682d432d29dd5bfd
Vladislav Moskovets <github@vlad.org.ua>
2013-07-07 21:45:00 +0300

    up

--- bashrc.d/terminal.sh
+++ bashrc.d/terminal.sh
@@ -11,11 +11,10 @@
 case "$TERM" in
 	xterm*|rxvt*|screen*)
     if [ `id -u` = 0 ]; then
-        PS1='${debian_chroot:+($debian_chroot)}\[\033[0;31m\]\u\[\033[00m\]@\[\033[0;32m\]\h\[\033[00m\] \[\033[00m\]\w\[\033[00m\] \$ '
+        PS1='\[\033[0;31m\]\h\[\033[00m\] \[\033[00m\]\w\[\033[00m\] \$ '
     else
-        PS1='${debian_chroot:+($debian_chroot)}\[\033[0;32m\]\u@\h\[\033[00m\] \[\033[00m\]\w\[\033[00m\] \$ '
+        PS1='\[\033[0;32m\]\u@\h\[\033[00m\] \[\033[00m\]\w\[\033[00m\] \$ '
     fi
-	#PROMPT_COMMAND='echo -ne "\033]0;${USER}@${HOSTNAME}: ${PWD/$HOME/~}\007"'
     TITLEBAR='\[\033]0;\u@\h:\w\007\]'
 	;;
 	*)
