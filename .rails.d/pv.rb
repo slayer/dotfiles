b04a6f94cbea349dd8bf57c1db34f321a845cc46
Vladislav Moskovets <github@vlad.org.ua>
2015-03-10 02:06:14 +0300

    1.8 hashes for ics

--- .rails.d/pv.rb
+++ .rails.d/pv.rb
@@ -5,15 +5,15 @@ end
 def sf id = nil
   case id
   when Integer
-    Survey::MR.where(id: id).first
+    Survey::MR.where(:id => id).first
   when String
     if id =~ /\d+-\d+/
-      Survey::MR.where(origin_id: id).first
+      Survey::MR.where(:origin_id => id).first
     else
       Survey::MR.where(:title.like id).first
     end
   when Array
-    Survey::MR.where(id: id)
+    Survey::MR.where(:id => id)
   else
     Survey::MR.last
   end
