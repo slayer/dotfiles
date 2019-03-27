00fa3ef397ea580d59da278745cdcbfb9b63a041
Vlad <devvlad@gmail.com>
2019-03-27 16:29:39 +0200

    git shortcut completions

--- .bashrc.d/git-shortcuts.sh
+++ .bashrc.d/git-shortcuts.sh
@@ -4,11 +4,11 @@
 ##################
 # git section
 #
-alias g="git "
+alias g="git ";                       [[ "$(type -t _git)" = "function" ]] && complete -o default -F _git g
 alias gs='git status'
 alias gst='git status'
 alias gsub='git submodule'
-alias gd='git diff'
+alias gd='git diff';                  [[ "$(type -t _git_diff)" = "function" ]] && complete -o default -F _git_diff gd
 alias gdc='git diff --cached'
 alias gdh='git diff HEAD'
 alias ga='git add'
@@ -16,13 +16,16 @@ alias gc='git commit -v'
 alias gci='git commit -v -m'
 alias gcia='git commit --amend'
 alias gca='gc -a'
-alias gb='git branch'
-alias gl='git log'
-alias glp='git log -p'
+alias gb='git branch';                [[ "$(type -t _git_branch)" = "function" ]] && complete -o default -F _git_branch gb
+alias gl='git log';                   [[ "$(type -t _git_log)" = "function" ]] && complete -o default -F _git_log gl
+alias glp='git log -p';               [[ "$(type -t _git_log)" = "function" ]] && complete -o default -F _git_log gl
 alias gpl='git pull origin master'
-alias gp='git push'
-alias gpp='git pull; git push'
-alias gcp='git cherry-pick'
+alias gp='git push -u'
+alias gpp='git pull; git push -u'
+
+
+
+[[ "$(type -t _git_log)" = "function" ]] && complete -o default -F _git_log glp
 
 function gco {
 	if [ -z "$1" ]; then
